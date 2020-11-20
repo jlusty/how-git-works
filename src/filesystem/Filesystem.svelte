ea011f2411cf100996c82a6ca505afddd70af896
John Lusty <54030459+jlusty@users.noreply.github.com>
2020-11-20 21:16:29 +0000

    Persist opened folder to local storage

--- src/filesystem/Filesystem.svelte
+++ src/filesystem/Filesystem.svelte
@@ -7,6 +7,7 @@
   const unsubscribe = foldername.subscribe((value) => {
     if (value.length > 0) {
       filesystem.set(readFolderToFilesystem(value));
+      localStorage.setItem("foldername", value);
     }
   });
 
